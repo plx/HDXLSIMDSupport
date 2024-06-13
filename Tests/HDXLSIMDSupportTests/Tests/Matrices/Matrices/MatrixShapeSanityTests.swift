a48f87ba7a34218ddd18c7e80441c9f9387dc91e
plx <plxgithub@gmail.com>
2024-06-12 20:15:36 -0500

    Ported shape-validation tests to Swift testing.

--- Tests/HDXLSIMDSupportTests/Tests/Matrices/Matrices/MatrixShapeSanityTests.swift
+++ Tests/HDXLSIMDSupportTests/Tests/Matrices/Matrices/MatrixShapeSanityTests.swift
@@ -1,366 +1,182 @@
-//
-//  MatrixShapeSanityTests.swift
-//
-
-import XCTest
+import Testing
 import simd
 @testable import HDXLSIMDSupport
 
-class MatrixShapeSanityTests: XCTestCase {
+func validateShape<Matrix>(
+  matrixType matrix: Matrix.Type,
+  columnCount: Int,
+  rowCount: Int
+) where Matrix: MatrixProtocol {
+  #expect(rowCount == matrix.rowCount)
+  #expect(columnCount == matrix.columnCount)
+  #expect(rowCount == matrix.columnLength)
+  #expect(columnCount == matrix.rowLength)
+  #expect(columnCount * rowCount == matrix.scalarCount)
+}
 
-  // ------------------------------------------------------------------------ //
-  // MARK: Shape Support
-  // ------------------------------------------------------------------------ //
-  
-  internal func validateShape<Matrix:MatrixProtocol>(
-    forMatrix matrix: Matrix.Type,
-    columnCount: Int,
-    rowCount: Int) {
-    
-    XCTAssertEqual(
-      rowCount,
-      matrix.rowCount,
-      "`\(String(reflecting: matrix))` had row-count mismatch: need \(rowCount), got \(matrix.rowCount)!"
-    )
-    XCTAssertEqual(
-      columnCount,
-      matrix.columnCount,
-      "`\(String(reflecting: matrix))` had column-count mismatch: need \(columnCount), got \(matrix.columnCount)!"
-    )
-    XCTAssertEqual(
-      rowCount,
-      matrix.columnLength,
-      "`\(String(reflecting: matrix))` had rowCount-columnLength mismatch: expect `rowCount == matrix.columnLength`, but got `rowCount: \(rowCount), matrix.columnLength: \(matrix.columnLength)`!"
-    )
-    XCTAssertEqual(
-      columnCount,
-      matrix.rowLength,
-      "`\(String(reflecting: matrix))` had columnCoun-rowLength mismatch: expect `columnCount == matrix.rowLength`, but got `columnCoun: \(columnCount), matrix.rowLength: \(matrix.rowLength)`!"
-    )
-    XCTAssertEqual(
-      rowCount * columnCount,
-      matrix.scalarCount,
-      "`\(String(reflecting: matrix))` had scalar-count mismatch: expect `rowCount * columnCount == matrix.scalarCount`, but got `rowCount * columnCount: \(rowCount * columnCount), matrix.scalarCount: \(matrix.scalarCount)`!"
+func validateCommonShape<each Matrix: MatrixProtocol>(
+  columnCount: Int,
+  rowCount: Int,
+  forMatrixTypes matrixTypes: (repeat (each Matrix).Type)
+) {
+  #expect(columnCount > 1)
+  #expect(rowCount > 1)
+  for matrixType in repeat each matrixTypes {
+    validateShape(
+      matrixType: matrixType,
+      columnCount: columnCount,
+      rowCount: rowCount
     )
   }
+}
 
-  // ------------------------------------------------------------------------ //
-  // MARK: (2, _) Shapes
-  // ------------------------------------------------------------------------ //
-  
-  func testMatrix2x2Shape() {
-    // A matrix of two columns and two rows...
-    self.validateShape(
-      forMatrix: simd_double2x2.self,
-      columnCount: 2,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: simd_float2x2.self,
-      columnCount: 2,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix2x2Storage.self,
-      columnCount: 2,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix2x2Storage.self,
-      columnCount: 2,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: Matrix2x2<Double>.self,
-      columnCount: 2,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: Matrix2x2<Float>.self,
-      columnCount: 2,
-      rowCount: 2
-    )
-  }
+// MARK: 2xN Shape Validation
 
-  func testMatrix2x3Shape() {
-    // A matrix of two columns and three rows...
-    self.validateShape(
-      forMatrix: simd_double2x3.self,
-      columnCount: 2,
-      rowCount: 3
+@Test("2x2 Matrix Shape Validation")
+func validate2x2MatrixShapes() {
+  validateCommonShape(
+    columnCount: 2,
+    rowCount: 2,
+    forMatrixTypes: (
+      simd_double2x2.self,
+      simd_float2x2.self,
+      DoubleMatrix2x2Storage.self,
+      FloatMatrix2x2Storage.self,
+      Matrix2x2<Double>.self,
+      Matrix2x2<Float>.self
     )
-    self.validateShape(
-      forMatrix: simd_float2x3.self,
-      columnCount: 2,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix2x3Storage.self,
-      columnCount: 2,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix2x3Storage.self,
-      columnCount: 2,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: Matrix2x3<Double>.self,
-      columnCount: 2,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: Matrix2x3<Float>.self,
-      columnCount: 2,
-      rowCount: 3
-    )
-  }
+  )
+}
 
-  func testMatrix2x4Shape() {
-    // A matrix of two columns and four rows...
-    self.validateShape(
-      forMatrix: simd_double2x4.self,
-      columnCount: 2,
-      rowCount: 4
+@Test("2x3 Matrix Shape Validation")
+func validate2x3MatrixShapes() {
+  validateCommonShape(
+    columnCount: 2,
+    rowCount: 3,
+    forMatrixTypes: (
+      simd_double2x3.self,
+      simd_float2x3.self,
+      DoubleMatrix2x3Storage.self,
+      FloatMatrix2x3Storage.self,
+      Matrix2x3<Double>.self,
+      Matrix2x3<Float>.self
     )
-    self.validateShape(
-      forMatrix: simd_float2x4.self,
-      columnCount: 2,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix2x4Storage.self,
-      columnCount: 2,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix2x4Storage.self,
-      columnCount: 2,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: Matrix2x4<Double>.self,
-      columnCount: 2,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: Matrix2x4<Float>.self,
-      columnCount: 2,
-      rowCount: 4
-    )
-  }
+  )
+}
 
-  // ------------------------------------------------------------------------ //
-  // MARK: (3, _) Shapes
-  // ------------------------------------------------------------------------ //
-  
-  func testMatrix3x2Shape() {
-    // A matrix of three columns and two rows...
-    self.validateShape(
-      forMatrix: simd_double3x2.self,
-      columnCount: 3,
-      rowCount: 2
+@Test("2x4 Matrix Shape Validation")
+func validate2x4MatrixShapes() {
+  validateCommonShape(
+    columnCount: 2,
+    rowCount: 4,
+    forMatrixTypes: (
+      simd_double2x4.self,
+      simd_float2x4.self,
+      DoubleMatrix2x4Storage.self,
+      FloatMatrix2x4Storage.self,
+      Matrix2x4<Double>.self,
+      Matrix2x4<Float>.self
     )
-    self.validateShape(
-      forMatrix: simd_float3x2.self,
-      columnCount: 3,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix3x2Storage.self,
-      columnCount: 3,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix3x2Storage.self,
-      columnCount: 3,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: Matrix3x2<Double>.self,
-      columnCount: 3,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: Matrix3x2<Float>.self,
-      columnCount: 3,
-      rowCount: 2
-    )
-  }
-  
-  func testMatrix3x3Shape() {
-    // A matrix of three columns and three rows...
-    self.validateShape(
-      forMatrix: simd_double3x3.self,
-      columnCount: 3,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: simd_float3x3.self,
-      columnCount: 3,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix3x3Storage.self,
-      columnCount: 3,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix3x3Storage.self,
-      columnCount: 3,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: Matrix3x3<Double>.self,
-      columnCount: 3,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: Matrix3x3<Float>.self,
-      columnCount: 3,
-      rowCount: 3
-    )
-  }
-  
-  func testMatrix3x4Shape() {
-    // A matrix of two columns and four rows...
-    self.validateShape(
-      forMatrix: simd_double3x4.self,
-      columnCount: 3,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: simd_float3x4.self,
-      columnCount: 3,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix3x4Storage.self,
-      columnCount: 3,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix3x4Storage.self,
-      columnCount: 3,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: Matrix3x4<Double>.self,
-      columnCount: 3,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: Matrix3x4<Float>.self,
-      columnCount: 3,
-      rowCount: 4
-    )
-  }
+  )
+}
 
-  // ------------------------------------------------------------------------ //
-  // MARK: (4, _) Shapes
-  // ------------------------------------------------------------------------ //
-  
-  func testMatrix4x2Shape() {
-    // A matrix of three columns and two rows...
-    self.validateShape(
-      forMatrix: simd_double4x2.self,
-      columnCount: 4,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: simd_float4x2.self,
-      columnCount: 4,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix4x2Storage.self,
-      columnCount: 4,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix4x2Storage.self,
-      columnCount: 4,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: Matrix4x2<Double>.self,
-      columnCount: 4,
-      rowCount: 2
-    )
-    self.validateShape(
-      forMatrix: Matrix4x2<Float>.self,
-      columnCount: 4,
-      rowCount: 2
-    )
-  }
-  
-  func testMatrix4x3Shape() {
-    // A matrix of three columns and three rows...
-    self.validateShape(
-      forMatrix: simd_double4x3.self,
-      columnCount: 4,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: simd_float4x3.self,
-      columnCount: 4,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix4x3Storage.self,
-      columnCount: 4,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix4x3Storage.self,
-      columnCount: 4,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: Matrix4x3<Double>.self,
-      columnCount: 4,
-      rowCount: 3
-    )
-    self.validateShape(
-      forMatrix: Matrix4x3<Float>.self,
-      columnCount: 4,
-      rowCount: 3
-    )
-  }
-  
-  func testMatrix4x4Shape() {
-    // A matrix of two columns and four rows...
-    self.validateShape(
-      forMatrix: simd_double4x4.self,
-      columnCount: 4,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: simd_float4x4.self,
-      columnCount: 4,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: DoubleMatrix4x4Storage.self,
-      columnCount: 4,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: FloatMatrix4x4Storage.self,
-      columnCount: 4,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: Matrix4x4<Double>.self,
-      columnCount: 4,
-      rowCount: 4
-    )
-    self.validateShape(
-      forMatrix: Matrix4x4<Float>.self,
-      columnCount: 4,
-      rowCount: 4
-    )
-  }
+// MARK: 3xN Shape Validation
 
+@Test("3x2 Matrix Shape Validation")
+func validate3x2MatrixShapes() {
+  validateCommonShape(
+    columnCount: 3,
+    rowCount: 2,
+    forMatrixTypes: (
+      simd_double3x2.self,
+      simd_float3x2.self,
+      DoubleMatrix3x2Storage.self,
+      FloatMatrix3x2Storage.self,
+      Matrix3x2<Double>.self,
+      Matrix3x2<Float>.self
+    )
+  )
+}
+
+@Test("3x3 Matrix Shape Validation")
+func validate3x3MatrixShapes() {
+  validateCommonShape(
+    columnCount: 3,
+    rowCount: 3,
+    forMatrixTypes: (
+      simd_double3x3.self,
+      simd_float3x3.self,
+      DoubleMatrix3x3Storage.self,
+      FloatMatrix3x3Storage.self,
+      Matrix3x3<Double>.self,
+      Matrix3x3<Float>.self
+    )
+  )
+}
+
+@Test("3x4 Matrix Shape Validation")
+func validate3x4MatrixShapes() {
+  validateCommonShape(
+    columnCount: 3,
+    rowCount: 4,
+    forMatrixTypes: (
+      simd_double3x4.self,
+      simd_float3x4.self,
+      DoubleMatrix3x4Storage.self,
+      FloatMatrix3x4Storage.self,
+      Matrix3x4<Double>.self,
+      Matrix3x4<Float>.self
+    )
+  )
+}
+
+// MARK: 4xN Shape Validation
+
+@Test("4x2 Matrix Shape Validation")
+func validate4x2MatrixShapes() {
+  validateCommonShape(
+    columnCount: 4,
+    rowCount: 2,
+    forMatrixTypes: (
+      simd_double4x2.self,
+      simd_float4x2.self,
+      DoubleMatrix4x2Storage.self,
+      FloatMatrix4x2Storage.self,
+      Matrix4x2<Double>.self,
+      Matrix4x2<Float>.self
+    )
+  )
+}
+
+@Test("4x3 Matrix Shape Validation")
+func validate4x3MatrixShapes() {
+  validateCommonShape(
+    columnCount: 4,
+    rowCount: 3,
+    forMatrixTypes: (
+      simd_double4x3.self,
+      simd_float4x3.self,
+      DoubleMatrix4x3Storage.self,
+      FloatMatrix4x3Storage.self,
+      Matrix4x3<Double>.self,
+      Matrix4x3<Float>.self
+    )
+  )
+}
+
+@Test("4x4 Matrix Shape Validation")
+func validate4x4MatrixShapes() {
+  validateCommonShape(
+    columnCount: 4,
+    rowCount: 4,
+    forMatrixTypes: (
+      simd_double4x4.self,
+      simd_float4x4.self,
+      DoubleMatrix4x4Storage.self,
+      FloatMatrix4x4Storage.self,
+      Matrix4x4<Double>.self,
+      Matrix4x4<Float>.self
+    )
+  )
 }
 
