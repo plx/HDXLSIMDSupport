8515b438d03182567c65de00c3b9c47fbfd93806
plx <plxgithub@gmail.com>
2024-06-12 20:44:59 -0500

    Ported MatrixPosition basic tests to Swift Testing.

--- Tests/HDXLSIMDSupportTests/Tests/Matrices/Position/MatrixPositionBasicTests.swift
+++ Tests/HDXLSIMDSupportTests/Tests/Matrices/Position/MatrixPositionBasicTests.swift
@@ -1,207 +1,89 @@
-//
-//  MatrixPositionBasicTests.swift
-//
-
-import XCTest
+import Testing
 import simd
 @testable import HDXLSIMDSupport
 
-class MatrixPositionBasicTests: XCTestCase {
+let testPositions = matrixPositions(
+  rowIndices: 0...5,
+  columnIndices: 0...5
+)
   
-  // ------------------------------------------------------------------------ //
-  // MARK: Test Management
-  // ------------------------------------------------------------------------ //
-  
-  override func setUp() {
-    super.setUp()
-    self.continueAfterFailure = false
-  }
-  
-  override func tearDown() {
-    super.tearDown()
-    self.continueAfterFailure = true
-  }
-  
-  let positions: [MatrixPosition] = [
-    [
-      MatrixPosition(rowIndex: 0, columnIndex: 0),
-      MatrixPosition(rowIndex: 0, columnIndex: 1),
-      MatrixPosition(rowIndex: 0, columnIndex: 2),
-      MatrixPosition(rowIndex: 0, columnIndex: 3),
-      MatrixPosition(rowIndex: 0, columnIndex: 4),
-      MatrixPosition(rowIndex: 0, columnIndex: 5)
-    ],
-    [
-      MatrixPosition(rowIndex: 1, columnIndex: 0),
-      MatrixPosition(rowIndex: 1, columnIndex: 1),
-      MatrixPosition(rowIndex: 1, columnIndex: 2),
-      MatrixPosition(rowIndex: 1, columnIndex: 3),
-      MatrixPosition(rowIndex: 1, columnIndex: 4),
-      MatrixPosition(rowIndex: 1, columnIndex: 5)
-    ],
-    [
-      MatrixPosition(rowIndex: 2, columnIndex: 0),
-      MatrixPosition(rowIndex: 2, columnIndex: 1),
-      MatrixPosition(rowIndex: 2, columnIndex: 2),
-      MatrixPosition(rowIndex: 2, columnIndex: 3),
-      MatrixPosition(rowIndex: 2, columnIndex: 4),
-      MatrixPosition(rowIndex: 2, columnIndex: 5)
-    ],
-    [
-      MatrixPosition(rowIndex: 3, columnIndex: 0),
-      MatrixPosition(rowIndex: 3, columnIndex: 1),
-      MatrixPosition(rowIndex: 3, columnIndex: 2),
-      MatrixPosition(rowIndex: 3, columnIndex: 3),
-      MatrixPosition(rowIndex: 3, columnIndex: 4),
-      MatrixPosition(rowIndex: 3, columnIndex: 5)
-    ],
-    [
-      MatrixPosition(rowIndex: 4, columnIndex: 0),
-      MatrixPosition(rowIndex: 4, columnIndex: 1),
-      MatrixPosition(rowIndex: 4, columnIndex: 2),
-      MatrixPosition(rowIndex: 4, columnIndex: 3),
-      MatrixPosition(rowIndex: 4, columnIndex: 4),
-      MatrixPosition(rowIndex: 4, columnIndex: 5)
-    ],
-    [
-      MatrixPosition(rowIndex: 5, columnIndex: 0),
-      MatrixPosition(rowIndex: 5, columnIndex: 1),
-      MatrixPosition(rowIndex: 5, columnIndex: 2),
-      MatrixPosition(rowIndex: 5, columnIndex: 3),
-      MatrixPosition(rowIndex: 5, columnIndex: 4),
-      MatrixPosition(rowIndex: 5, columnIndex: 5)
-    ]
-  ].flatMap() { $0 }
-  
-  // ------------------------------------------------------------------------ //
-  // MARK: Position Tests
-  // ------------------------------------------------------------------------ //
-  
-  func testValidity() {
-    self.positions.forEach() {
-      XCTAssertTrue($0.isValid)
-    }
-  }
-  
-  func testEquality() {
-    for (lIndex,lhs) in self.positions.enumerated() {
-      for (rIndex,rhs) in self.positions.enumerated() {
-        // TODO: really want a `AssertEquivalentEquality`,
-        // but still gathering ideas for an `HDXLTestSupport` package.
-        if lIndex == rIndex {
-          XCTAssertEqual(lhs,rhs)
-        } else {
-          XCTAssertNotEqual(lhs, rhs)
-        }
-      }
-    }
-  }
-  
-  func testDiagonal() {
-    self.positions.forEach() {
-      XCTAssertEqual(
-        $0.rowIndex == $0.columnIndex,
-        $0.isOnDiagonal
-      )
-      XCTAssertEqual(
-        $0.rowIndex != $0.columnIndex,
-        $0.isOffDiagonal
-      )
-    }
-  }
-  
-  func testTransposition() {
-    for position in self.positions {
-      
-      var clone = position
-      clone.formTranspose()
-      XCTAssertEqual(
-        clone.rowIndex,
-        position.columnIndex
-      )
-      XCTAssertEqual(
-        clone.columnIndex,
-        position.rowIndex
-      )
-      
-      let transposed = position.transposed()
-      XCTAssertEqual(
-        clone,
-        transposed
-      )
-      
-      let doublyTransposed = transposed.transposed()
-      XCTAssertEqual(
-        position,
-        doublyTransposed
-      )
-      
-      clone.formTranspose()
-      XCTAssertEqual(
-        position,
-        clone
-      )
 
-    }
-  }
-  
-  func testWithRowIndex() {
-    for position in self.positions {
-      for rowIndex in 0...5 where rowIndex != position.rowIndex && rowIndex != position.columnIndex {
-        XCTAssertNotEqual(
-          rowIndex,
-          position.rowIndex
-        )
-        XCTAssertNotEqual(
-          rowIndex,
-          position.columnIndex
-        )
-        
-        let result = position.with(rowIndex: rowIndex)
-        XCTAssertEqual(
-          rowIndex,
-          result.rowIndex
-        )
-        XCTAssertNotEqual(
-          rowIndex,
-          result.columnIndex
-        )
-        XCTAssertEqual(
-          position.columnIndex,
-          result.columnIndex
-        )
-      }
-    }
-  }
-
-  func testWithColumnIndex() {
-    for position in self.positions {
-      for columnIndex in 0...5 where columnIndex != position.rowIndex && columnIndex != position.columnIndex {
-        XCTAssertNotEqual(
-          columnIndex,
-          position.rowIndex
-        )
-        XCTAssertNotEqual(
-          columnIndex,
-          position.columnIndex
-        )
-        
-        let result = position.with(columnIndex: columnIndex)
-        XCTAssertEqual(
-          columnIndex,
-          result.columnIndex
-        )
-        XCTAssertNotEqual(
-          columnIndex,
-          result.rowIndex
-        )
-        XCTAssertEqual(
-          position.rowIndex,
-          result.rowIndex
-        )
-      }
-    }
-  }
-
+@Test(
+  "`MatrixPosition.isValid` for known-valid positions.",
+  arguments: testPositions
+)
+func goodMatrixPositionsAreValid(position: MatrixPosition) {
+  #expect(position.isValid)
 }
 
+@Test(
+  "`MatrixPosition` diagonal calculations are correct.",
+  arguments: testPositions
+)
+func testMatrixPositionDiagonalChecks(
+  value: MatrixPosition
+) {
+  #expect(
+    (value.isOnDiagonal)
+    ==
+    (value.rowIndex == value.columnIndex)
+  )
+  #expect(
+    (value.isOffDiagonal)
+    ==
+    (value.rowIndex != value.columnIndex)
+  )
+  #expect(
+    value.isOnDiagonal == !value.isOffDiagonal
+  )
+}
+
+@Test(
+  "`MatrixPosition.with(rowIndex:)` works as-expected.",
+  arguments: testPositions, 6...9
+)
+func testMatrixPositionWithRowIndex(
+  value: MatrixPosition,
+  newRowIndex: Int
+) {
+  #expect(newRowIndex != value.rowIndex)
+  let updatedValue = value.with(rowIndex: newRowIndex)
+  #expect(updatedValue.rowIndex == newRowIndex)
+  #expect(updatedValue.columnIndex == value.columnIndex)
+}
+
+@Test(
+  "`MatrixPosition.with(columnIndex:)` works as-expected.",
+  arguments: testPositions, 6...9
+)
+func testMatrixPositionWithRowIndex(
+  value: MatrixPosition,
+  newColumnIndex: Int
+) {
+  #expect(newColumnIndex != value.columnIndex)
+  let updatedValue = value.with(columnIndex: newColumnIndex)
+  #expect(updatedValue.rowIndex == value.rowIndex)
+  #expect(updatedValue.columnIndex == newColumnIndex)
+}
+
+@Test(
+  "`MatrixPosition` transposition is correct and coherent.",
+  arguments: testPositions
+)
+func testMatrixPositionWithRowIndex(
+  value: MatrixPosition
+) {
+  let transposed = value.transposed()
+  #expect(value.rowIndex == transposed.columnIndex)
+  #expect(value.columnIndex == transposed.rowIndex)
+  
+  let doublyTransposed = transposed.transposed()
+  #expect(doublyTransposed == value)
+  
+  var clone = value
+  #expect(clone == value)
+  clone.formTranspose()
+  #expect(clone == transposed)
+  clone.formTranspose()
+  #expect(clone == value)
+}
